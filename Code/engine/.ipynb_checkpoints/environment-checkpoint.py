f3aed4217d19e4b7721a1d118c551c0ae6c09b94
Elliot Alderson <iammrrobot24@gmail.com>
2021-06-30 23:15:13 +0200

    Automatic License Plate Detection

--- Code/engine/.ipynb_checkpoints/environment-checkpoint.py
+++ Code/engine/.ipynb_checkpoints/environment-checkpoint.py
@@ -2,38 +2,53 @@ import imutils
 import cv2
 from skimage.segmentation import clear_border
 import pytesseract
+import skimage.io as io
 import numpy as np
 import matplotlib.pyplot as plt
-
+from itertools import accumulate
+import os
+import os.path
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import ImageGrid
+from math import *
 
 """
-Input: 
-img1: First Image
-img2: Second Image
-title1: Title for First Image
-title2: Title for Second Image
------------------------------
+Read a dataset a of images
+-------------------
+Return a list of images
+
 """
+def read_images(path_name,no_cars=426,ext='png'):
+    images = []
+    for i in range(no_cars):
+        car_label = path_name.format(i,ext)
+        if os.path.isfile(car_label):
+            images.append(io.imread(car_label))
+        elif os.path.isfile(path_name.format(i,"jpg")):
+            images.append(path_name.format(i,"jpg"))
+        elif os.path.isfile(path_name.format(i,"JPG")):
+            images.append(path_name.format(i,"JPG"))
+    return images
 
-def plot_images(img1,img2,title1="",title2=""):
-    fig = plt.figure(figsize=(15,15))
-    ax1 = fig.add_subplot(121) # 121 - 1 Row 2 Columns and Target 1st Column of Row
-    ax1.imshow(img1,cmap='gray')
-    ax1.set(xticks=[],yticks=[],title=title1)
-    ax2 = fig.add_subplot(122) # 121 - 1 Row 2 Columns and Target 1st Column of Row
-    ax2.imshow(img2,cmap='gray')
-    ax2.set(xticks=[],yticks=[],title=title2) 
-    fig.show()
     
-
-
     
+    
+"""
+Create histogram distribution to see the intensity values and investigate if we cannot stretch of equalize the intensities to make the image lighter
+"""
+def create_image_histogram(gray):
+    hist,bins = np.histogram(gray.flatten(),256,[0,256])
+    cdf = hist.cumsum()
+    cdf_normalized = cdf * float(hist.max()) / cdf.max()
+    return cdf_normalized
     
 def get_blackhat(gray,kernel_width=13,kernel_height=5,kernel=None): # Reveal Dark Regions (Letter,Digits,Symbols) on Light Backgrounds
     if kernel == None:
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(kernel_width,kernel_height))
     blackhat = cv2.morphologyEx(gray.copy(),cv2.MORPH_BLACKHAT,kernel)
     return blackhat,kernel
+
+
 
 
 def get_closing(gray,kernel_width=3,kernel_height=3,kernel=None): # Fill Small Holes and Identify Larger Structures: Reveal Light Characters
@@ -65,3 +80,72 @@ def morphological_preprocessing(gray,square_kernel_width=3,square_kernel_height=
     light_threshold = cv2.dilate(light_threshold,None,iterations=2)
     light_threshold = cv2.erode(light_threshold,None,iterations=1)
     return light_threshold.copy()
+
+
+
+#===================================================================== Light Manipulation ==================================
+
+def hist_eq_(img): # Perform Histogram Equalization
+    unique_elements, counts_elements = np.unique(img, return_counts=True)
+    cdf_ = list(accumulate(counts_elements))
+    hv_eq = lambda cdf_v,cdf_min,M,N,L:np.round(((cdf_v-cdf_min)/(M*N - cdf_min))*(L-1))
+    M,N = img.shape
+    intensity = 256
+    hv = [hv_eq(cdfv,min(cdf_),M,N,intensity) for cdfv in cdf_]
+    hv_eq = lambda cdf_v,cdf_min,M,N,L:np.round(((cdf_v-cdf_min)/(M*N - cdf_min))*(L-1)).astype(int)
+    hv = [hv_eq(cdfv,min(cdf_),M,N,intensity) for cdfv in cdf_]
+    i_ = np.zeros((M,N))
+    for x,y in zip(unique_elements,hv):
+        i_ = np.where(img==x,y,i_)
+    return i_
+
+def adjust_gamma(image, gamma=1.0):
+    invGamma = 1.0 / gamma
+    table = np.array([((i / 255.0) ** invGamma) * 255
+        for i in np.arange(0, 256)]).astype("uint8")
+    return cv2.LUT(image, table)
+#===================================================================== Plotting Images===================================================================== 
+
+def image_random_sample(images,k=16):
+    image_data = images[:k]
+    fig = plt.figure(figsize=(26, 20))
+    grid = ImageGrid(fig, 111,  # similar to subplot(111)
+                 nrows_ncols=(int(sqrt(k)), int(sqrt(k))),  # creates 2x2 grid of axes
+                 axes_pad=0.1,)  # pad between axes in inch.)
+
+    for ax, im in zip(grid, image_data):
+        ax.imshow(im)
+
+    plt.title("Random Sample of Images")
+    plt.show()
+
+def plot_histogram(gray,cdf_normalized):
+    plt.plot(cdf_normalized, color = 'b')
+    plt.hist(gray.flatten(),256,[0,256], color = 'r')
+    plt.xlim([0,256])
+    plt.title("Histogram Plot of the Grayscale Image")
+    plt.xlabel("Intensity")
+    plt.ylabel("Count")
+    plt.legend(('cdf','histogram'), loc = 'upper left')
+    plt.show()
+    
+    
+"""
+Input: 
+img1: First Image
+img2: Second Image
+title1: Title for First Image
+title2: Title for Second Image
+-----------------------------
+"""
+
+def plot_images(img1,img2,title1="",title2=""):
+    fig = plt.figure(figsize=(15,15))
+    ax1 = fig.add_subplot(121) # 121 - 1 Row 2 Columns and Target 1st Column of Row
+    ax1.imshow(img1,cmap='gray')
+    ax1.set(xticks=[],yticks=[],title=title1)
+    ax2 = fig.add_subplot(122) # 121 - 1 Row 2 Columns and Target 1st Column of Row
+    ax2.imshow(img2,cmap='gray')
+    ax2.set(xticks=[],yticks=[],title=title2) 
+    fig.show()
+    
--- Code/engine/license_detection.py
+++ Code/engine/license_detection.py
@@ -9,6 +9,7 @@ import imutils
 import pytesseract
 import numpy as np
 import re
+min_aspect_ratio = 2;max_aspect_ratio = 8
 
 """
 Task: Check the ratio of the minimum bounding rectangle that is drawn by the contour
@@ -151,6 +152,101 @@ def get_license_text(plate_image):
     text = str(re.split('\n',text)[0])
     return text
     
-def license_plate_detection(type_="type1",verbose=False):
-    pass
+#============================ Generalized License Plate Detection With Hyperparameter Tuning
+"""
+Perform stepwise license plate detection
+"""
+def detect_license_plate(img,gray,contours,min_aspect_ratio=min_aspect_ratio,max_aspect_ratio=max_aspect_ratio,verbose=False):
+    morphological_image = en.morphological_preprocessing(gray)
+    morphological_contours = find_license_plate_contours(morphological_image)
+    plate,found,__ = find_license_plate(img,morphological_contours,min_aspect_ratio=min_aspect_ratio,max_aspect_ratio=max_aspect_ratio,verbose=False)
+    if (found == False) or len(get_license_text(plate)) <= 2:
+        if verbose:
+            print("Morphological Operation: No License Plate Detected")
+            print("Attempt License Plate Detection Using Canny Edge Detection .....")
+            print("==============================================================")
+        blur = cv2.bilateralFilter(gray,11,90,90)
+        edges = cv2.Canny(blur,30,200)
+        contours = find_license_plate_contours(edges)
+        _,found,__ = find_license_plate(img,contours,min_aspect_ratio=min_aspect_ratio,max_aspect_ratio=max_aspect_ratio,verbose=False)#(image,cnts,precision=0.02,min_aspect_ratio=4,max_aspect_ratio=8,closed=True,verbose=False)
+        if found == True and len(get_license_text(_)) >=2:
+            if verbose:
+                print("License Plate Detection Using Canny Edge Detection Operation: License Plate Detected")
+            plate = _
 
+        for pt in __:
+            if verbose:
+                print("Checking Possible Plates:")
+            if len(get_license_text(pt)) >2:
+                plate = pt # License Text = '\x0c'
+                found = True
+                plt.imshow(plate,cmap='gray')
+                return plate,found
+
+        if (found == False) or len(get_license_text(plate)) <= 2:
+            if verbose:
+                print("License Plate Detection Using Canny Edge Detection Operation: No License Plate Detected")
+                print("Attempt License Plate Detection Using Canny Edge Detection By Blurring The Canny Edge Image .....")
+                print("==============================================================")
+            edges_blur = cv2.GaussianBlur(edges,(5,5),0)
+            contours = find_license_plate_contours(edges_blur)
+            _,found,__ = find_license_plate(img,contours,min_aspect_ratio=min_aspect_ratio,max_aspect_ratio=max_aspect_ratio,verbose=False)
+            if found == True and len(get_license_text(_)) > 2:
+                plate = _    
+            else:
+                if verbose:
+                    print("No License Plate")
+                plate = img.copy()
+                found = False 
+                return plate,found
+    return plate,found
+
+
+
+"""
+Given a list of gamma values, loop through each one and illuminate or darken the image and detect a license
+Return an image that best approximates a license plate if it exists
+"""
+def gamma_hyperparameter_tuning(img,gray,contours,min_aspect_ratio=min_aspect_ratio,max_aspect_ratio=max_aspect_ratio,verbose=True):
+    if verbose:
+        print("Performing Gamma Hyper parameter tuning")
+    gammas = np.linspace(0.5,1,6)
+    plate = img.copy()
+    found = False
+    for gamma in gammas:
+        adjusted_gray = en.adjust_gamma(gray,gamma)
+        morphological_image = en.morphological_preprocessing(adjusted_gray)
+        morphological_contours = find_license_plate_contours(morphological_image)
+        plate,found = detect_license_plate(img,adjusted_gray,morphological_contours,min_aspect_ratio=min_aspect_ratio,max_aspect_ratio=max_aspect_ratio,verbose=verbose)
+        if found:
+            break
+    return plate,found,gamma
+
+
+
+"""
+Adjust the intensities of the image based on the hyperparameters of the histogram equalization, gamma values, and normal blurred gray image.
+If the license plate exists, then perform license plate detection on all these hyperparameters and choose the best one that approximates the license plate
+if it does not exist then return the image itself
+
+"""
+def hyperparameter_tuning(img,gray,contours,min_aspect_ratio=min_aspect_ratio,max_aspect_ratio=max_aspect_ratio,verbose=True):
+    hist = en.hist_eq_(gray).astype('uint8')
+    hist_tuning = detect_license_plate(img,hist,contours,min_aspect_ratio=min_aspect_ratio,max_aspect_ratio=max_aspect_ratio,verbose=verbose)
+    gray_tuning = detect_license_plate(img,gray,contours,min_aspect_ratio=min_aspect_ratio,max_aspect_ratio=max_aspect_ratio,verbose=verbose)
+    gamma_tuning = gamma_hyperparameter_tuning(img,gray,contours,min_aspect_ratio=min_aspect_ratio,max_aspect_ratio=max_aspect_ratio,verbose=verbose)
+    
+    hist_plates = (hist_tuning[0],hist_tuning[1],len(get_license_text(hist_tuning[0])))
+    gray_plates = (gray_tuning[0],gray_tuning[1],len(get_license_text(gray_tuning[0])))
+    gamma_plates = (gamma_tuning[0],gamma_tuning[1],len(get_license_text(gamma_tuning[0])))
+    plates = [hist_plates,gray_plates,gamma_plates]
+    max_ = 0
+    current_plate = None
+    for best_plate in plates:
+        if best_plate[1] == True and best_plate[2] > max_:
+            current_plate = best_plate
+            max_ = best_plate[2]
+    if current_plate == None:
+        return img.copy(),False
+    else:
+        return current_plate,True
